4532ee2b2df357aaa4d7bd5b00a53a36b901485c
lilyh14 <lhinkeldey@ufl.edu>
2019-12-01 23:25:20 -0500

    story 20 works - without the business ID for internships

--- client/src/views/businessListings.js
+++ client/src/views/businessListings.js
@@ -21,7 +21,7 @@ class businessListings extends React.Component {
     }
 
     loadBusinessInternships() {
-        axios.get(config.apiURL + "Internship/" + this.state.view ).then(results => { //Internship business ID? 
+        axios.get(config.apiURL + "Internship/" ).then(results => { //Internship business ID? 
             console.log(results);
             var listings = results.data;
             if (listings) {
@@ -47,17 +47,25 @@ class businessListings extends React.Component {
     }
 
     render() {
+        
         if(!this.state.loading){
             return (
+                <div className="container"  >
+                    
                 <div>
                     <ul>
+                    <div className="form-group">
+                    <label htmlFor="name">Click to edit specific internship</label>
                         {this.state.listings.map((listing, index) => (
-                            <li key={index}><a href={'/listing/view/' + listing.business_id}>{listing.title}</a></li>
+                            <li key={index}><a href={'/listing/edit/' + listing._id}>{listing.title}</a></li>  //Link is clicking on it that goes to the specific internship page
+                   
                         ))}
+                        
+                        </div>
                     </ul>
 
+                    </div>
                 </div>
-
             );
         }else{
             return (<p>Loading...</p>);
