ae8c33d2d5e4d650490888e800ce4c16773a5e1d
lilyh14 <lhinkeldey@ufl.edu>
2019-12-02 22:55:46 -0500

    Build

--- client/src/config.js
+++ client/src/config.js
@@ -2,7 +2,7 @@
     var port = process.env.PORT || 3000;
 
 const config = {
-    apiURL: "http://localhost" + port + "/api/",
+    apiURL: "https://spect-internship.herokuapp.com/api/",
     stripePub: 'pk_test_hZc2lACYs3t8AU3uoGafa706',
     listingPrice: '$20'
 }
