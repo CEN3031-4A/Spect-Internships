2534ee4e1cd466a56cb093761d718f1fff5a52d7
lilyh14 <lhinkeldey@ufl.edu>
2019-12-05 21:26:46 -0500

    porting

--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ const express = require('./config/express.js')
 // Use env port or default
 
 //const port = process.env.PORT || 5000;
-const port = 5008;
-
+//const port = 5008;
+const port = process.env.PORT;
 
 
 const app = express.init()
